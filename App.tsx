853734c85ee060f28570e3397d0c8067a228b948
Danilo Ribeiro <danilo.ribeiro00d@gmail.com>
2021-10-04 22:11:45 -0300

    Added status bar style

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { Home } from './src/pages/Home';
 
 export default function App() {
 	return (
 		<>
 			<Home />
+			<StatusBar style="auto" />
 		</>
 	);
 }
